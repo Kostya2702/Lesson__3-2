f02c6480f9f4f7b9dc126ef5234471628cf21d63
Kostya2702 <loshkarev.kostya27@gmail.com>
2020-11-24 08:50:24 -0800

    Осталось добавить движение

--- Lesson_3-2/Lesson_3-2/ViewController.swift
+++ Lesson_3-2/Lesson_3-2/ViewController.swift
@@ -9,29 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var indicatorTopBottom: NSLayoutConstraint!
+    
+    private lazy var panGestureRecognizer: UIPanGestureRecognizer = UIPanGestureRecognizer (
+        target: self,
+        action: #selector(rangeNumber(_:))
+    )
 
     override func viewDidLoad() {
         super.viewDidLoad()
         addNewLabel()
+        addNewView()
     }
     
     func addNewLabel() {
         
         let topLabel = UILabel()
+        topLabel.text = "Hello world!"
+        topLabel.backgroundColor = .gray
         topLabel.translatesAutoresizingMaskIntoConstraints = false
+        topLabel.textColor = .white
         view.addSubview(topLabel)
         
         topLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        topLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
-        topLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
+    }
+    
+    func addNewView() {
         
-        topLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50).isActive = true
+        let topView = UIView()
+        topView.backgroundColor = .blue
+        topView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(topView)
         
-        topLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 150).isActive = true
+        topView.topAnchor.constraint(equalTo: view.topAnchor, constant: 160).isActive = true
+        topView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
+        topView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50).isActive = true
+        topView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        topView.heightAnchor.constraint(equalToConstant: 128).isActive = true
         
         
     }
 
-
+    @objc func rangeNumber(_ sender: UIPanGestureRecognizer) {
+        
+        
+        
+    }
+    
 }
 
